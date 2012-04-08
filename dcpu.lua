136c740ed9e26c4f903313448698d7effb03522a
Asim <asim@chuhnk.me>
2012-04-08 23:12:56 +0100

    it works\!

--- dcpu.lua
+++ dcpu.lua
@@ -1,6 +1,15 @@
 require "bit"
 require "disassemble"
 
+DEBUG = true
+
+
+function debug(msg)
+  if DEBUG then 
+    print(msg)
+  end
+end
+
 function dump_header()
   print(" PC   SP   OV  SKIP  A    B    C    X    Y    Z    I    J   instruction")
   print("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- -----------")
@@ -8,14 +17,16 @@ end
 
 function dump_state(cpu)
   local _,st = disassemble(cpu.mem, cpu.pc)
-  print(string.format("%04x %04x %04x %s %04x %04x %04x %04x %04x %04x %04x %04x %s", 
-	cpu.pc, cpu.sp, cpu.ov, tostring(cpu.skip), cpu.reg[0], cpu.reg[1], 
+  local sp = string.sub(string.format("%04x", cpu.sp), 1, 4)
+  print(string.format("%04x %s %04x %s %04x %04x %04x %04x %04x %04x %04x %04x %s", 
+	cpu.pc, sp, cpu.ov, tostring(cpu.skip), cpu.reg[0], cpu.reg[1], 
 	cpu.reg[2], cpu.reg[3], cpu.reg[4], cpu.reg[5], cpu.reg[6], 
 	cpu.reg[7], st))
 end
 
 function dcpu()
   local ram_size = 0x10000
+  local literals = 32
   local cpu = {}
   local registers = 8
 
@@ -25,7 +36,7 @@ function dcpu()
   cpu.ov = 0
   cpu.mem = {}
   cpu.reg = {}
-  cpu.st = {}
+  cpu.lit = {}
 
   for i=0,registers-1 do
     cpu.reg[i] = 0
@@ -35,48 +46,92 @@ function dcpu()
     cpu.mem[i] = 0
   end
 
+  for i=0,literals-1 do
+    cpu.lit[i] = i
+  end
+
   return cpu
 end
 
-function get_op(cpu, code)
-  if code < 0x08 then
-    return cpu.reg[code]
-  elseif code < 0x10 then
-    return cpu.mem[cpu.reg[bit.band(code, 7)]]
-  elseif code < 0x18 then
-    local val = bit.band(cpu.reg[bit.band(code, 7)] + cpu.mem[cpu.pc], 0xFFFF)
+function get_op(cpu, op)
+  local reg = "reg"
+  local mem = "mem"
+  local lit = "lit"
+  local ov = "ov"
+  local pc = "pc"
+  local sp = "sp"
+
+  debug(string.format("pc: %s op: %s", cpu.pc, op))
+
+  if op < 0x08 then
+    return reg, op
+  elseif op < 0x10 then
+    return mem, cpu.reg[bit.band(op, 7)]
+  elseif op < 0x18 then
+    local val = bit.band(cpu.reg[bit.band(op, 7)] + cpu.mem[cpu.pc], 0xFFFF)
     cpu.pc = cpu.pc + 1
-    return cpu.mem[val]
-  elseif code == 0x18 then
+    return mem, val
+  elseif op == 0x18 then
     local val = cpu.sp
     cpu.sp = cpu.sp + 1
-    return cpu.mem[val]
-  elseif code == 0x19 then
-    return cpu.mem[cpu.sp]
-  elseif code == 0x1a then
+    return mem, val
+  elseif op == 0x19 then
+    return mem, cpu.sp
+  elseif op == 0x1A then
     cpu.sp = cpu.sp - 1
-    return cpu.mem[cpu.sp]
-  elseif code == 0x1b then
-    return cpu.sp
-  elseif code == 0x1c then
-    return cpu.pc
-  elseif code == 0x1d then
-    return cpu.ov
-  elseif code == 0x1e then
+    return mem, cpu.sp
+  elseif op == 0x1B then
+    return sp, cpu.sp
+  elseif op == 0x1C then
+    return pc, cpu.pc
+  elseif op == 0x1D then
+    return ov, cpu.ov
+  elseif op == 0x1E then
+    debug(string.format("0x1e %s", cpu.pc))
+
     local val = cpu.mem[cpu.pc]
     cpu.pc = cpu.pc + 1
-    return cpu.mem[val]
-  elseif code == 0x1f then
+    return mem, val
+  elseif op == 0x1F then
+    debug(string.format("0x1f %s", cpu.pc))
+
     local val = cpu.pc
     cpu.pc = cpu.pc + 1
-    return cpu.mem[val]
+    return mem, val
   else
-    return code - 0x20
+    return lit, (op - 0x20) % 32
   end 
 end
 
+function read(cpu, op, val)
+  if op == "mem" then
+    return cpu.mem[val]
+  elseif op == "reg" then
+    return cpu.reg[val]
+  elseif op == "pc" then
+    return cpu.pc
+  elseif op == "sp" then
+    return cpu.sp
+  elseif op == "ov" then
+    return cpu.ov
+  elseif op == "lit" then
+    return val
+  end
+end
+
+function write(cpu, op, val, value)
+  if op == "mem" then
+    cpu.mem[val] = value
+  elseif op == "reg" then
+    cpu.reg[val] = value
+  elseif op == "pc" then
+    debug(string.format("write pc: %s", value))
+    cpu.pc = value
+  end
+end
+
 function step(cpu)
-  local dst, res, ma, mb, cop, op, opcode
+  local a, b, dst, loc, res, ma, mb, cop, op, opcode
 
   op = cpu.mem[cpu.pc]
   cpu.pc = cpu.pc + 1
@@ -86,14 +141,16 @@ function step(cpu)
 
   if opcode == 0 then
     if cop == 0x01 then
-      ma = get_op(cpu, bit.brshift(op, 10)) 
-
+      aloc,ma = get_op(cpu, bit.brshift(op, 10)) 
+      ma = read(cpu, aloc, ma)
+      
       if cpu.skip == true then
         cpu.skip = false
       else
         cpu.sp = cpu.sp - 1
         cpu.mem[cpu.sp] = cpu.pc
         cpu.pc = ma
+        debug(string.format("cpu.sp %04x cpu.pc %s ma %s", cpu.sp, cpu.pc, ma))
       end
     
       return
@@ -103,9 +160,15 @@ function step(cpu)
     end
   end 
 
+  res = 0
   dst = cop
-  ma = get_op(cpu, dst)
-  mb = get_op(cpu, bit.brshift(op, 10))
+  aloc, a = get_op(cpu, dst)
+  bloc, b = get_op(cpu, bit.brshift(op, 10))
+  
+  ma = read(cpu, aloc, a) 
+  mb = read(cpu, bloc, b)
+
+  debug(string.format("ma %s mb %s",ma, mb))
 
   if opcode == 0x01 then -- SET
     res = mb
@@ -120,9 +183,9 @@ function step(cpu)
   elseif opcode == 0x06 then -- MOD
     if mb == 0 then res = 0 else res = ma % mb end
   elseif opcode == 0x07 then -- SHL
-    res = tonumber(bit.blshift(ma, mb))
+    res = bit.blshift(ma, mb)
   elseif opcode == 0x08 then -- SHR 
-    res = tonumber(bit.brshift(ma,mb))
+    res = bit.brshift(ma, mb)
   elseif opcode == 0x09 then -- AND
     res = bit.band(ma, mb)
   elseif opcode == 0x0A then -- BOR
@@ -130,13 +193,13 @@ function step(cpu)
   elseif opcode == 0x0B then -- XOR
     res = bit.bxor(ma, mb)
   elseif opcode == 0x0C then -- IFE
-    res = ma == mb  
+    res = ma ~= mb  
   elseif opcode == 0x0D then -- IFN
-    res = ma ~= mb
+    res = ma == mb
   elseif opcode == 0x0E then -- IFG
-    res = ma > mb 
+    res = ma <= mb 
   elseif opcode == 0x0F then -- IFB
-    res = bit.band(ma, mb) ~= 0
+    res = bit.band(ma, mb) == 0
   else
     print("illegal op")
     os.exit(1)
@@ -147,18 +210,14 @@ function step(cpu)
     return
   end
  
- if opcode == 0x02 and opcode <= 0x08 then
-    cpu.ov = bit.brshift(res, 16)
-  elseif opcode == 0x01 or
-         opcode == 0x06 or
-         opcode == 0x09 or
-         opcode == 0x0A or
-         opcode == 0x0B then
-
-    if (dst < 0x1f) then cpu.reg[bit.band(dst, 7)] = res end
-  elseif opcode >= 0x0C and opcode <= 0x0F then
+  if opcode >= 0x0C and opcode <= 0x0F then
+    debug(string.format("res skip %s", tostring(res)))
     cpu.skip = res
-  end   
+  else
+    debug(string.format("setting cpu[%s][%s] = %s", aloc, a, res))
+    write(cpu, aloc, a, res)
+    cpu.ov = bit.band(bit.brshift(res, 16), 0xFFFF)
+  end
 end
 
 function load(cpu, filename)
@@ -172,7 +231,6 @@ function load(cpu, filename)
       cpu.mem[n] = bytes
       n = n + 1
     end
-    
   end
 
   f:close()
@@ -221,9 +279,9 @@ function main()
     filename = arg[1]
   end
 
-  -- load(dcpu, filename)
+  load(dcpu, filename)
 
-   mem_set(dcpu)
+  -- mem_set(dcpu)
 
   dump_header() 
   while true do
